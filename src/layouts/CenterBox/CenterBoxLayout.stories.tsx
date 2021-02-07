d51471bdc3990a5daf3fe86c935964d22d40aad3
kimromi <kimromi4@gmail.com>
2021-02-07 16:49:30 +0900

    add CenterBoxLayout story with ThemeProvider

---
+++ src/layouts/CenterBox/CenterBoxLayout.stories.tsx
@@ -0,0 +1,29 @@
+import { ThemeProvider } from "styled-components";
+import merge from "deepmerge";
+
+import { theme } from "../../theme";
+import { CenterBoxLayout } from "./CenterBoxLayout";
+import { Text, Button } from "../../components";
+
+export default {
+  component: CenterBoxLayout,
+  title: "Layouts/Center Box",
+};
+
+const centerBoxTheme = merge(theme, {
+  box: {
+    width: "300px",
+    margin: "195px 0 100px 0",
+    padding: "30px",
+    textAlign: "center",
+  },
+});
+
+export const CenterBox = (): React.ReactElement => (
+  <ThemeProvider theme={centerBoxTheme}>
+    <CenterBoxLayout>
+      <Text tag="p">Text</Text>
+      <Button color="primary">Button</Button>
+    </CenterBoxLayout>
+  </ThemeProvider>
+);
