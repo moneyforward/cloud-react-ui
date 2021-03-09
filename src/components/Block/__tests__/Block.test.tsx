8e4d43f47f0411fcee130bd943abf7f973255aaf
kimromi <kimromi4@gmail.com>
2021-03-09 23:07:21 +0900

    add Block test

---
+++ src/components/Block/__tests__/Block.test.tsx
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+import { Block } from "../Block";
+
+describe("Block", () => {
+  it("snapshot test", () => {
+    const { asFragment } = render(<Block>block</Block>);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  describe("border props", () => {
+    it("border=true", () => {
+      const { container } = render(<Block border>block</Block>);
+      expect(container.firstChild).toHaveStyle("border: 1px solid #d4d8dd;");
+    });
+
+    it("border=false", () => {
+      const { container } = render(<Block>block</Block>);
+      expect(container.firstChild).toHaveStyle("border: none;");
+    });
+  });
+});
