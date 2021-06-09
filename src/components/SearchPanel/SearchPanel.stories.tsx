bdfa70fa8aa0dc05ab47b74e08dec17f9793f09a
Yoshiro Matsumoto <matsumoto.yoshiro@moneyforward.co.jp>
2021-06-09 15:35:42 +0900

    create inner components
    - SearchPanel.Body
    - SearchPanel.Actions

--- src/components/SearchPanel/SearchPanel.stories.tsx
+++ src/components/SearchPanel/SearchPanel.stories.tsx
@@ -2,6 +2,16 @@ import { Story, Meta } from "@storybook/react";
 import { SearchPanel } from "./SearchPanel";
 import type { SearchPanelProps } from "./SearchPanel";
 
-const Template: Story<SearchPanelProps> = (args) => <SearchPanel {...args} />;
+export default {
+  component: SearchPanel,
+  title: "components/SearchPanel",
+} as Meta;
+
+const Template: Story<SearchPanelProps> = ({ children }) => (
+  <SearchPanel>
+    <SearchPanel.Body>{children}</SearchPanel.Body>
+    <SearchPanel.Actions>Button</SearchPanel.Actions>
+  </SearchPanel>
+);
 
 export const Default = Template.bind({});
--- src/components/SearchPanel/SearchPanel.tsx
+++ src/components/SearchPanel/SearchPanel.tsx
@@ -1,11 +1,39 @@
 import styled, { css } from "styled-components";
+import { color } from "../../theme";
 
-const StyledSearchPanel = styled.div``;
+const SearchPanelBody = styled.div`
+  width: 100%;
+  padding-top: 15px;
+  padding-bottom: 15px;
+`;
+SearchPanelBody.displayName = "SearchPanel.Body";
+
+const SearchPanelActions = styled.div`
+  width: 100%;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  border-top: 1px solid ${color.linkWater};
+  display: flex;
+  justify-content: center;
+`;
+SearchPanelActions.displayName = "SearchPanel.Actions";
+
+const StyledSearchPanel = styled.div`
+  width: 100%;
+  padding-left: 20px;
+  padding-right: 20px;
+  background-color: #fff;
+`;
 
 export type SearchPanelProps = {
   children?: React.ReactNode;
 };
 
-export const SearchPanel = ({ children }: SearchPanelProps) => (
+export const SearchPanelBase = ({ children }: SearchPanelProps) => (
   <StyledSearchPanel>{children}</StyledSearchPanel>
 );
+
+export const SearchPanel = Object.assign(SearchPanelBase, {
+  Body: SearchPanelBody,
+  Actions: SearchPanelActions,
+});
