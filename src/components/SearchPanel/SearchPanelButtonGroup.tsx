2169e030ac58588412f268247e03fc9c6dc88482
Yoshiro Matsumoto <matsumoto.yoshiro@moneyforward.co.jp>
2021-06-10 13:28:48 +0900

    buttonを等幅にする

--- src/components/SearchPanel/SearchPanelButtonGroup.tsx
+++ src/components/SearchPanel/SearchPanelButtonGroup.tsx
@@ -5,6 +5,10 @@ export const SearchPanelButtonGroup = styled.div`
   display: flex;
   justify-content: center;
   column-gap: 8px;
+
+  & > button {
+    min-width: 76px;
+  }
 `;
 
 SearchPanelButtonGroup.displayName = "SearchPanel.ButtonGroup";
