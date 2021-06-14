71ec215deb6c186597ba386ccca99b8099a4a425
Yoshiro Matsumoto <matsumoto.yoshiro@moneyforward.co.jp>
2021-06-14 09:11:33 +0900

    remove unnecessary elements

--- src/components/SearchPanel/__tests__/SearchPanel.test.tsx
+++ src/components/SearchPanel/__tests__/SearchPanel.test.tsx
@@ -14,34 +14,6 @@ describe("SearchPanel", () => {
                 <TextField id="text-field-1" />
               </div>
             </SearchPanel.Column>
-            <SearchPanel.Column>
-              <div>
-                <label htmlFor="text-field-1">Text label</label>
-                <TextField id="text-field-1" />
-              </div>
-            </SearchPanel.Column>
-          </SearchPanel.Row>
-          <SearchPanel.Row>
-            <SearchPanel.Column>
-              <fieldset>
-                <legend>Checkbox group</legend>
-                <Checkbox>checkbox 1</Checkbox>
-                <Checkbox>checkbox 2</Checkbox>
-                <Checkbox>checkbox 3</Checkbox>
-                <Checkbox>checkbox 4</Checkbox>
-                <Checkbox>checkbox 5</Checkbox>
-              </fieldset>
-            </SearchPanel.Column>
-            <SearchPanel.Column>
-              <fieldset>
-                <legend>Checkbox group</legend>
-                <Checkbox>checkbox 1</Checkbox>
-                <Checkbox>checkbox 2</Checkbox>
-                <Checkbox>checkbox 3</Checkbox>
-                <Checkbox>checkbox 4</Checkbox>
-                <Checkbox>checkbox 5</Checkbox>
-              </fieldset>
-            </SearchPanel.Column>
           </SearchPanel.Row>
         </SearchPanel.Filters>
         <SearchPanel.Actions>
