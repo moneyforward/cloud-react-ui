d0b1aaa8b1cd728b3e9253153ca065608737c629
Masaya Nakazato <nakazato.masaya@moneyforward.co.jp>
2021-06-23 14:44:32 +0900

    updated tests

--- src/components/Dropdown/__tests__/Dropdown.test.tsx
+++ src/components/Dropdown/__tests__/Dropdown.test.tsx
@@ -1,58 +1,108 @@
-import { render } from "@testing-library/react";
-import { Dropdown, DropdownProps } from "../../Dropdown";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { Dropdown, DropdownBodyProps } from "../../Dropdown";
 import { Link } from "../../../components";
+import { useState } from "react";
+
+type Props = Pick<DropdownBodyProps, "placement">;
 
 describe("Dropdown", () => {
-  const Template = (args: DropdownProps): JSX.Element => (
-    <Dropdown {...args}>
-      <Dropdown.Block>
-        <Dropdown.Item>
-          <Link href="#">ユーザー設定</Link>
-        </Dropdown.Item>
-        <Dropdown.Item>
-          <Link href="#">ユーザー設定</Link>
-        </Dropdown.Item>
-        <Dropdown.Item>
-          <Link href="#">ユーザー設定</Link>
-        </Dropdown.Item>
-      </Dropdown.Block>
-    </Dropdown>
-  );
+  const Template = (args: Props): JSX.Element => {
+    const [isOpen, setIsOpen] = useState(false);
 
-  it("default", () => {
-    const { asFragment } = render(<Template toggleLabel="ラベル" />);
+    const handleToggle = () => {
+      setIsOpen(!isOpen);
+    };
+
+    const handleClose = () => {
+      setIsOpen(false);
+    };
+    return (
+      <Dropdown>
+        <Dropdown.ToggleButton onClick={handleToggle} ariaExpanded={isOpen}>
+          ButtonLabel
+        </Dropdown.ToggleButton>
+        <Dropdown.Body
+          width="200px"
+          ariaHidden={!isOpen}
+          placement={args["placement"]}
+        >
+          <Dropdown.Block>
+            <Dropdown.Item collapsed onClick={handleClose}>
+              <Link href="#">Link</Link>
+            </Dropdown.Item>
+            <Dropdown.Item collapsed onClick={handleClose}>
+              <Link href="#">Link</Link>
+            </Dropdown.Item>
+            <Dropdown.Item collapsed onClick={handleClose}>
+              <Link href="#">Link</Link>
+            </Dropdown.Item>
+          </Dropdown.Block>
+        </Dropdown.Body>
+      </Dropdown>
+    );
+  };
+
+  it("default", async () => {
+    const { asFragment } = render(<Template />);
+    fireEvent.click(screen.getByRole("button"));
+    expect(screen.getAllByRole("link")).toHaveLength(3);
     expect(asFragment()).toMatchSnapshot();
   });
 
-  it("with placement", () => {
-    const { asFragment } = render(
-      <Template toggleLabel="ラベル" placement="right" />
-    );
+  it("with placement", async () => {
+    const { asFragment } = render(<Template placement="right" />);
+    fireEvent.click(screen.getByRole("button"));
+    expect(screen.getAllByRole("link")).toHaveLength(3);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("close", async () => {
+    const { asFragment } = render(<Template />);
+    fireEvent.click(screen.getByRole("button"));
+
+    await waitFor(() => {
+      fireEvent.click(screen.getAllByText("Link")[0]);
+      expect(screen.queryAllByRole("link")).not.toHaveLength(3);
+    });
+
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("with multiple blocks", () => {
-    const { asFragment } = render(
-      <Dropdown toggleLabel="ラベル">
-        <Dropdown.Block>
-          Blockを複数配置することで、内容を分割できます。
-        </Dropdown.Block>
-        <Dropdown.Block>
-          <Dropdown.Item>
-            <Link href="#">ユーザー設定</Link>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <Link href="#">ユーザー設定</Link>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <Link href="#">ユーザー設定</Link>
-          </Dropdown.Item>
-        </Dropdown.Block>
-        <Dropdown.Block>
-          Blockを複数配置することで、内容を分割できます。
-        </Dropdown.Block>
-      </Dropdown>
-    );
+    const MultipleTemplate = (): JSX.Element => {
+      const [isOpen, setIsOpen] = useState(false);
+
+      const handleToggle = () => {
+        setIsOpen(!isOpen);
+      };
+      return (
+        <Dropdown>
+          <Dropdown.ToggleButton onClick={handleToggle} ariaExpanded={isOpen}>
+            ButtonLabel
+          </Dropdown.ToggleButton>
+          <Dropdown.Body width="200px" ariaHidden={!isOpen}>
+            <Dropdown.Block>Block</Dropdown.Block>
+            <Dropdown.Block>
+              <Dropdown.Item collapsed>
+                <Link href="#">Link</Link>
+              </Dropdown.Item>
+              <Dropdown.Item collapsed>
+                <Link href="#">Link</Link>
+              </Dropdown.Item>
+              <Dropdown.Item collapsed>
+                <Link href="#">Link</Link>
+              </Dropdown.Item>
+            </Dropdown.Block>
+            <Dropdown.Block>Block</Dropdown.Block>
+          </Dropdown.Body>
+        </Dropdown>
+      );
+    };
+
+    const { asFragment } = render(<MultipleTemplate />);
+    fireEvent.click(screen.getByRole("button"));
+    expect(screen.getAllByText("Block")).toHaveLength(2);
+    expect(screen.getAllByRole("link")).toHaveLength(3);
     expect(asFragment()).toMatchSnapshot();
   });
 });
