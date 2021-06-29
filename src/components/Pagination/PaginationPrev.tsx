86ca3c6fba72ccb371042950a1c692838e41bdbf
Yoshiro Matsumoto <matsumoto.yoshiro@moneyforward.co.jp>
2021-06-29 18:36:00 +0900

    set aria-label

--- src/components/Pagination/PaginationPrev.tsx
+++ src/components/Pagination/PaginationPrev.tsx
@@ -11,7 +11,13 @@ export const PaginationPrev = forwardRef<
   HTMLAnchorElement,
   PaginationPrevProps
 >(({ href, disabled, ...rest }, ref) => (
-  <PaginationLink href={href} ref={ref} disabled={disabled} {...rest}>
+  <PaginationLink
+    aria-label="前のページ"
+    href={href}
+    ref={ref}
+    disabled={disabled}
+    {...rest}
+  >
     <Icon icon="chevronUp" rotation={270} />
   </PaginationLink>
 ));
