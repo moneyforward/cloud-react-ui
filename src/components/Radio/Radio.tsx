6aad3b9332ade9decaae4df925c9692f6da1ba57
Yoshiro Matsumoto <matsumoto.yoshiro@moneyforward.co.jp>
2021-09-13 16:13:59 +0900

    markup変更前と同等の振る舞いまで調整

--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -15,51 +15,32 @@ export type Props = {
   id: string;
 };
 
-const RadioWrapper = styled.span`
-  ${({ theme: { radio } }) => css`
-    position: relative;
-    display: inline-block;
-    width: ${radio.width};
-    height: ${radio.height};
-    margin-top: 2px;
-    margin-right: 4px;
-  `}
+const RadioWrapper = styled.div`
+  display: inline-flex;
+  align-items: center;
 `;
 RadioWrapper.defaultProps = defaultProps;
 
 const Input = styled.input`
-  ${({ theme: { radio } }) => css`
-    /* opacity: 0;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    cursor: pointer; */
-
-    :checked + ${RadioButton} {
-      background-color: ${radio.backgroundColor.checked};
-      border-width: ${radio.borderWidth.checked};
-      border-style: ${radio.borderStyle.checked};
-      border-color: ${radio.borderColor.checked};
-    }
-
-    :disabled + ${RadioButton} {
-      background-color: ${radio.backgroundColor.disabled};
-      border-width: ${radio.borderWidth.disabled};
-      border-style: ${radio.borderStyle.disabled};
-      border-color: ${radio.borderColor.disabled};
-    }
-  `}
+  /* visually-hidden */
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
 `;
 Input.defaultProps = defaultProps;
 
 const RadioButton = styled.span`
+  display: grid;
+  place-items: center;
+  margin-right: 4px;
+
   ${({ theme: { radio } }) => css`
-    /* position: absolute;
-    width: 100%;
-    height: 100%; */
+    width: 16px;
+    height: 16px;
     border-radius: ${radio.borderRadius};
     background-color: ${radio.backgroundColor.unchecked};
     border-width: ${radio.borderWidth.unchecked};
@@ -67,19 +48,29 @@ const RadioButton = styled.span`
     border-color: ${radio.borderColor.unchecked};
     /* pointer-events: none; */
     box-sizing: border-box;
+
+    ${Input}:checked + ${Label} > & {
+      background-color: ${radio.backgroundColor.checked};
+      border-width: ${radio.borderWidth.checked};
+      border-style: ${radio.borderStyle.checked};
+      border-color: ${radio.borderColor.checked};
+    }
+
+    ${Input}:disabled + ${Label} > & {
+      background-color: ${radio.backgroundColor.disabled};
+      border-width: ${radio.borderWidth.disabled};
+      border-style: ${radio.borderStyle.disabled};
+      border-color: ${radio.borderColor.disabled};
+    }
   `}
 `;
 RadioButton.defaultProps = defaultProps;
 
 const IconWrapper = styled.span`
   ${({ theme: { radio } }) => css`
-    position: absolute;
     display: inline-block;
-    top: 50%;
-    left: 50%;
     width: ${radio.icon.width};
     height: ${radio.icon.height};
-    transform: translate(-50%, -50%);
     /* pointer-events: none; */
 
     & > svg {
@@ -94,6 +85,7 @@ const Icon = styled(({ className }) => (
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 6 6"
     className={className}
+    aria-hidden="true"
   >
     <circle
       cx="102"
@@ -105,12 +97,7 @@ const Icon = styled(({ className }) => (
   </svg>
 ))`
   ${({ theme: { radio } }) => css`
-    width: ${radio.icon.width};
-    height: ${radio.icon.height};
-
-    & > circle {
-      fill: ${radio.icon.color};
-    }
+    fill: ${radio.icon.color};
   `}
 `;
 Icon.defaultProps = defaultProps;
@@ -118,33 +105,24 @@ Icon.defaultProps = defaultProps;
 const Label = styled.label`
   cursor: pointer;
   display: inline-flex;
-  align-items: flex-start;
+  align-items: center;
 `;
 
 const LabelText = styled.span``;
 
 const Radio = forwardRef<HTMLInputElement, Props>(
   ({ children, className, id, ...rest }, ref) => (
-    // <Label>
-    //   <RadioWrapper className={className}>
-    //     <Input type="radio" ref={ref} {...rest} />
-    //     <Box />
-    //     <IconWrapper>
-    //       <Icon />
-    //     </IconWrapper>
-    //   </RadioWrapper>
-    //   <LabelText>{children}</LabelText>
-    // </Label>
-    <div>
+    <RadioWrapper>
       <Input type="radio" id={id} ref={ref} {...rest} />
       <Label htmlFor={id}>
-        <RadioButton />
-        <IconWrapper>
-          <Icon />
-        </IconWrapper>
+        <RadioButton>
+          <IconWrapper>
+            <Icon />
+          </IconWrapper>
+        </RadioButton>
         <LabelText>{children}</LabelText>
       </Label>
-    </div>
+    </RadioWrapper>
   )
 );
 
