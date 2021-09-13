aa8ec65ccd0113facc5b7c1bc326875ea14a4159
Yoshiro Matsumoto <matsumoto.yoshiro@moneyforward.co.jp>
2021-09-13 19:53:52 +0900

    put a second radio to test behavor

--- src/components/Radio/Radio.stories.tsx
+++ src/components/Radio/Radio.stories.tsx
@@ -1,19 +1,49 @@
 import { Story, Meta } from '@storybook/react';
 import { Radio, Props } from './Radio';
+import styled from 'styled-components';
 
 export default {
   component: Radio,
   title: 'components/Radio',
+  argTypes: {
+    onClick: { action: 'clicked' },
+    onChange: { action: 'changed' },
+  },
 } as Meta;
 
-const Template: Story<Props> = (args) => <Radio {...args}>Radio</Radio>;
+const Wrapper = styled.div`
+  display: flex;
+  column-gap: 10px;
+`;
+
+const Template: Story<Props> = ({ id, name, onClick, onChange, ...args }) => (
+  <Wrapper>
+    <Radio
+      id="radio-1"
+      name={name}
+      onClick={onClick}
+      onChange={onChange}
+      {...args}
+    >
+      Radio 1
+    </Radio>
+    <Radio
+      id="radio-2"
+      name={name}
+      onClick={onClick}
+      onChange={onChange}
+      value="second"
+    >
+      Radio 2
+    </Radio>
+  </Wrapper>
+);
 
 export const Default = Template.bind({});
 Default.args = {
-  id: 'radio-1',
   name: 'default',
   value: 'value-1',
 };
 
 export const Disabled = Template.bind({});
-Disabled.args = { disabled: true, id: 'radio-2', name: 'disabled' };
+Disabled.args = { disabled: true, name: 'disabled' };
